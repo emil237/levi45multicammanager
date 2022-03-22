b402778b28a99b91ee6a1ccd6d773abe387ce473
emil237 <95539537+emil237@users.noreply.github.com>
2022-03-22 23:21:44 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -8,10 +8,10 @@
 echo "***********************************************************************************************************************"
 # Config script #
 TMPDIR='/tmp'
-VERSION='9.3'
+VERSION='9.4'
 MY_URL='https://raw.githubusercontent.com/emil237/levi45multicammanager/main'
-MY_IPK="levi45multicammanager_9.3_all.ipk"
-MY_DEB="levi45multicammanager_9.3_all.deb"
+MY_IPK="levi45multicammanager_9.4_all.ipk"
+MY_DEB="levi45multicammanager_9.4_all.deb"
 ####################
 MY_EM='============================================================================================================'
 #  Remove Old Plugin  #
@@ -41,7 +41,7 @@ if which dpkg > /dev/null 2>&1; then
 	fi
 echo "============================================================================================================================"
 set +e
-cd ..
+cd 
 wait
 rm -f /tmp/$MY_IPK
 rm -f /tmp/$MY_DEB
@@ -60,6 +60,8 @@ init 4
 wait
 init 3
 exit 0
+
+
 
 
 
